79f155a5556d3940ccc3cb38f8cc4a6a5120130a
Christian Barr <cbarr792@west-mec.org>
2024-08-13 17:20:33 -0700

    added previous code to christian.js

---
+++ starter/christian.js
@@ -0,0 +1,23 @@
+// Import the prompt-sync library to handle user input
+
+// Import the gravityFactors module which contains factors for different planets
+
+// Define a function to show user factors based on input type and value
+// Initialize an object to hold the results
+// Declare a variable to hold the unit of measurement
+// Iterate over each item in the gravityFactors object
+// Calculate the factor multiplied by the input value and round it to two decimals
+// Switch case to determine the measurement unit based on factor type
+// Iterate over the results and log each one
+
+// Define a function to get user inputs for factor type and value
+// Prompt the user to enter the type of factor they want to calculate
+// Prompt the user to enter the numerical value of the factor
+// Call the showUserFactors function with the user inputs and the gravity factors
+// Expose getUserFactors globally'
+
+function getUserInput() {
+    console.log(`what are you measuring?`);
+    const userType = prompt(">");
+    consoel.log(`your `)
+}
