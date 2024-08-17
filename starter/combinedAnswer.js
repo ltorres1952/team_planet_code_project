310b2878e3ba7c545490efbfc440bcac2e675d9a
Luis Torres <ltorre701@west-mec.org>
2024-08-16 17:05:54 -0700

    added measurement types(imperial, metric)

--- starter/combinedAnswer.js
+++ starter/combinedAnswer.js
@@ -1,10 +1,10 @@
 const prompt = require("prompt-sync")();
 const gravityFactors = require("./utils/earthGravityFactors");
 const alienGravityFactors = require("./utils/alienGravityFactors");
-const factorTypes = ['jump', 'weight'];
+const validWords = ['jump', 'weight'];
 
 
-function calculateValue(planetType, factorType, factorUnit) {
+function calculateValue(planetType, factorType, userMeasurement, factorUnit) {
     const factors = {};
     let measurement;
     let planetGravity = {};
@@ -20,23 +20,37 @@ function calculateValue(planetType, factorType, factorUnit) {
             }
             break;
     }
-
-
-
-
     // trim it, match case, break if input matches item in array
 
     switch (factorType) {
-        case 'jump':
-            measurement = 'cm';
+        case 'jump':   
+            if (userMeasurement == 'imperial') {
+                measurement = 'in'
+            } else if(userMeasurement == 'metric'){
+                measurement = 'cm'
+            }
             for (let planet in planetGravity) {
                 factors[planet] = parseFloat((factorUnit / planetGravity[planet]).toFixed(2));
             }
             break;
         case 'weight':
-            measurement = 'kg';
+            if (userMeasurement == 'imperial') {
+                measurement = 'lb'
+            } else if (userMeasurement == 'metric') {
+                measurement = 'kg'
+            }
             for (let planet in planetGravity) {
                 factors[planet] = parseFloat((factorUnit * planetGravity[planet]).toFixed(2));
+            }
+            break;
+        case 'pushups':
+            if (userMeasurement == 'imperial') {
+                measurement = 'in'
+            } else if (userMeasurement == 'metric') {
+                measurement = 'cm'
+            }
+            for (let planet in planetGravity) {
+                factors[planet] = parseFloat((factorUnit / planetGravity[planet]).toFixed(2));
             }
             break;
         default:
@@ -46,33 +60,38 @@ function calculateValue(planetType, factorType, factorUnit) {
     for (let planet in factors) {
         console.log(`Your ${factorType} on ${planet} is: ${factors[planet]}${measurement}`);
     }
-}
 
+}
+// parseFloat(userValue);
+// !isNaN(userValue);
 function userInput() {
-    console.log("What do you want to measure('weight' or 'jump'): ");
-    let userUnit = prompt(">");
     // console.log(`Enter system of measurement('imperial' or 'metric'`);
     // let userSystem = prompt(">");
     // console.log(`Enter value of ${userUnit}`);
     // let userMeasurement = prompt(">");
+    let isMatch;
+    console.log("What do you want to measure('weight' or 'jump'): ");
+    let userUnit = prompt(">");
+    // while (true) {
+    //     let userUnit = prompt(">");
+    //     userUnit = userUnit.trim().toLowerCase();
+    //     validWords.forEach((item) => { 
+    //         if (item == userUnit) {
+                
+    //         }
+            
+    //     })
+
+       
+    // }
+    
+    console.log(`What measurement system are you using?('metric' or 'imperial')`)
+    let userMeasurement = prompt(">");
     console.log(`Enter value of ${userUnit}`);
     let userValue = prompt(">");
-    for (let i = 0; i < factorTypes.length - 1; i++) {
-        parseFloat(userValue);
-        !isNaN(userValue);
-        userValue.trim();
-
-        while (true) {
-            if (!isNaN(userValue) == true || userValue == factorTypes[i]) {
-                break;
-            } else {
-                console.error("Not that");
-            }
-        }
-    }
     console.log("What planets do you want to measure on?('alien', 'earth')");
-    let planetType = prompt(">")
-    calculateValue(planetType, userUnit, userValue);
+    let planetType = prompt(">");
+    calculateValue(planetType, userUnit, userMeasurement, userValue);
 }
 global.calculateValue = calculateValue;
 global.userInput = userInput;
